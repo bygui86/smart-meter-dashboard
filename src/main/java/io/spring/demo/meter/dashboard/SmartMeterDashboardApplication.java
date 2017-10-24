82d2b621ee858f5cf855595f7e3840f8fd5d0057
Brian Clozel <bclozel@pivotal.io>
2017-10-24 16:04:47 +0200

    Create application with start.spring.io

---
+++ src/main/java/io/spring/demo/meter/dashboard/SmartMeterDashboardApplication.java
@@ -0,0 +1,12 @@
+package io.spring.demo.meter.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMeterDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMeterDashboardApplication.class, args);
+	}
+}
---
+++ src/test/java/io/spring/demo/meter/dashboard/SmartMeterDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package io.spring.demo.meter.dashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartMeterDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
